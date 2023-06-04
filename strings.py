22469c13d72164fbb7186aa031f494a2f2be1566
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-04 23:06:31 +0600

    using different functions and keywords in strings

---
+++ strings.py
@@ -0,0 +1,14 @@
+name = "Ramisa Zahara Matin"
+
+print(name.upper())
+print(name.lower())
+print(name)
+
+print(name.find('t'))
+
+print('t' in name)
+
+print('p' in name)
+
+
+print(name.replace("Ramisa Zahara Matin" , "CAT"))
