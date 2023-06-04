3e9f9171a6b41c4c615501a0865e1aa86b54ab09
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-04 22:42:05 +0600

    sum of two numbers in py

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,7 @@
+number1 = input("Enter the First number : ")
+number2= input("Enter the second number : ")
+
+sum= int(number1) + int(number2)
+
+print(sum)
+print("Sum of two numbers is : " + str(sum))
