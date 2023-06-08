2b35b8d0d245cd83d57def14d50c23b893f2398f
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-08 15:07:42 +0600

    Formatted String in py

---
+++ formatted_str.py
@@ -0,0 +1,10 @@
+name1 = "Joseph"
+name2 = "Smith"
+#Joseph [smith] likes black
+
+#not appropriate 
+str = name1 + ' [' + name2 + '] likes black'
+print(str)
+
+#Formatted String
+print( f"{name1} [{name2}] likes black" )
