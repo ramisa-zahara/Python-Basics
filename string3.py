caa66430933f1c2b7247c4ece963cc70b191c21e
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-08 14:59:43 +0600

    more details on string in py

---
+++ string3.py
@@ -0,0 +1,24 @@
+msg = "Its's a python basic git repository"
+msg2 = 'It is a "Python" basics git repository'
+print(msg)
+print(msg2)
+course = msg[0]
+print(course)
+
+course = msg[-1]
+print(course)
+
+course = msg[1:5]
+print(course)
+
+course = msg[ : 5]
+print(course)
+
+course = msg[1:-1]
+print(course)
+
+course = msg[ 6: ]
+print(course)
+
+
+
