13d50328722068733911f084af3c9e4556ca562d
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-19 23:25:16 +0600

    if, else if and else statements basic

---
+++ if_else_statement.py
@@ -0,0 +1,13 @@
+one_million_money = False
+two_millon = True
+
+if one_million_money:
+    print("Jackpottttttt Mannnn ")
+
+elif two_millon:
+    print("Bigggg Jackpotttt damnn")
+
+else:
+    print("Oh no bad luck")
+
+print("Whatever you get save money <3")
