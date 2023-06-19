c2662fa52b934b3af22854657d1c57b5356d65c6
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-06-19 23:15:58 +0600

    Updates math functions

--- math_functions.py
+++ math_functions.py
@@ -4,5 +4,16 @@ import math
 
 print(round(5.9776))
 print(abs(-3.657))
+
+
 print(math.floor(4.6877))
 print(math.ceil(4.6877))
+print(math.factorial(5))
+#factorial of X
+
+print(math.fmod(10,20))
+print(math.fabs(-4.6877))
+#abs() and math.fabs( ) both returns absolute values.
+
+print(math.copysign(5,-10))
+#copysign returns floating point with the value of X but the sign of Y
