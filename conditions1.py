48ac381d2c75047da9ff47ca9fb37f83f03ae33a
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-07-05 20:24:13 +0600

    conditionals with comparison operator

--- conditions1.py
+++ conditions1.py
@@ -2,7 +2,7 @@ name= input()
 if len(name)< 3:
     print("Name must be at least 3 characters")
 
-elif len(name)>50:
+elif len(name)>=50:
     print("name can be maximum of 50 characters")
 else:
     print("Otherwise")
