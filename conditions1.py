5b756b0c10f9342ca86d9691c1c361e77fce76ac
Ramisa Zahara Matin <ramisazahara@gmail.com>
2023-07-05 20:22:14 +0600

    use of if, else, if else

---
+++ conditions1.py
@@ -0,0 +1,8 @@
+name= input()
+if len(name)< 3:
+    print("Name must be at least 3 characters")
+
+elif len(name)>50:
+    print("name can be maximum of 50 characters")
+else:
+    print("Otherwise")
